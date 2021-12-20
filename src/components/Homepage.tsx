9d51b44be27030ea0906a688646dfed89ecc0a9e
Max <zeetreex@gmail.com>
2021-12-20 14:32:09 +0200

    homepage layout

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom"
+
+const { Title } = Typography;
 
 const Homepage: React.FC = () => {
     return (
         <div>
-            Homepage
+            <Title level={2} className="heading">Global Crypto Stats</Title>
+            <Row>
+                <Col span={12}>
+                    <Statistic title="Total cryptocurrencies" value="5" />
+                    <Statistic title="Total exchanges" value="5" />
+                    <Statistic title="Total market cap" value="5" />
+                    <Statistic title="Total 24h volume" value="5" />
+                    <Statistic title="Total markets" value="5" />
+                </Col>
+            </Row>
         </div>
     );
 };
