8a6070d253fb8a9464728e7e5b9a7134de60cd32
Max <zeetreex@gmail.com>
2021-12-21 10:39:41 +0200

    homepage and crypto page are done, search bar is working

--- src/components/Cryptocurrencies.tsx
+++ src/components/Cryptocurrencies.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import millify from "millify";
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd'
@@ -20,15 +20,28 @@ interface CryptocurrenciesProps {
     simplified: boolean
 }
 
-const Cryptocurrencies = () => {
-    const {data: cryptosList, isFetching } = useGetCryptosQuery();
-    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
-    console.log(cryptos)
+const Cryptocurrencies = ( { simplified }: CryptocurrenciesProps ) => {
+    const count = simplified ? 10 : 100
+    const {data: cryptosList, isFetching } = useGetCryptosQuery(count);
+    const [cryptos, setCryptos] = useState<[]>()
+    const [searchTerm, setSearchTerm] = useState('')
+
+
+
+    useEffect( () => {
+        const filteredData = cryptosList?.data?.coins.filter( (item: { name: string }) => item.name.toLowerCase().includes(searchTerm))
+        setCryptos(filteredData)
+    }, [searchTerm, cryptosList] )
 
     return (
         <div>
-             <Row gutter={[32, 32]} className="crypto-card-container" >
-                {cryptos.map( (currency: ICurrency) => (
+            {!simplified &&  <div className="search-crypto">
+                <Input placeholder="Search Cryptocurrency" onChange={ (e) => setSearchTerm(e.target.value.toLowerCase())}/>
+            </div>}
+
+
+            {isFetching ? "loading..." : <Row gutter={[32, 32]} className="crypto-card-container" >
+                {cryptos?.map( (currency: ICurrency) => (
                     <Col xs={ 24 } sm={ 12 } lg={ 6 } className="crypto-card" key={currency.id}>
                         <Link to={`/crypto/${currency.uuid}`}>
                             <Card title={`${currency.rank}. ${currency.name}`}
@@ -42,7 +55,7 @@ const Cryptocurrencies = () => {
                         </Link>
                     </Col>
                 ))}
-            </Row>
+            </Row>}
         </div>
     );
 };
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -12,7 +12,7 @@ const { Title } = Typography;
 
 
 const Homepage: React.FC = () => {
-    const { data, isFetching } = useGetCryptosQuery();
+    const { data, isFetching } = useGetCryptosQuery(10);
     const globalStats = data?.data?.stats;
 
 
@@ -36,7 +36,7 @@ const Homepage: React.FC = () => {
                     <Link to="/cryptocurrencies">Show More</Link>
                 </Title>
             </div>
-            <Cryptocurrencies />
+            <Cryptocurrencies simplified />
 
             <div className="home-heading-container">
                 <Title level={2} className="home-title">Latest Crypto News</Title>
